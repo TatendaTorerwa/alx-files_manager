8f6ee7e5dc38f3acce1fe75b661af52d04a4c8e4
TatendaTorerwa <ttorerwa01@gmail.com>
2024-06-30 03:26:50 -0700

    3. Create a new user.

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,23 @@ import AppController from '../controllers/AppController';
 
 const router = express.Router();
 
+// App Controller
+
+// should return if Redis is alive and if the DB is alive
 router.get('/status', (req, res) => {
   AppController.getStatus(req, res);
 });
 
+// should return the number of users and files in DB
 router.get('/stats', (req, res) => {
   AppController.getStats(req, res);
 });
+
+// User Controller
+
+// should create a new user in DB
+router.post('/users', (req, res) => {
+  UsersController.postNew(req, res);
+  });
 
 export default router;
